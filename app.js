4c22ae647e6cc0c6837b4dde404b85121a387ce5
Deepak Kumar <deepak.kumar07@nagarro.com>
2022-02-25 23:56:56 +0530

    Node - Initial Commit

---
+++ app.js
@@ -0,0 +1,110 @@
+console.log('Hello World....');
+
+const temp = require('./temp');
+
+console.log(temp);
+
+console.log(temp.add(10,2));
+console.log(temp.mul(10,2));
+console.log(temp.div(10,2));
+// console.log(temp(1,2));
+
+
+
+// Built in modules
+
+// event
+// http
+// os
+// path
+// file reading
+// buffer
+// stream
+
+
+// import vs require
+// ES6 vs JS - babel
+// selective import -----not selective
+// asyn/non-blocker fetching --------- sync/blocker
+
+
+// module - ES6 - react
+
+
+// express - node framework - database conn
+
+
+
+// NODE - libraryXXXXXX
+// framework XXXXXXXXX
+// programming language XXXXXXXXX
+
+// node - platform - base for js
+
+// inbuilt features - to act as a back end server
+
+
+// v8 - javasript engine
+// C++ - coded
+
+
+// Java vs nodejs-------------------------------------------
+// 1) Java - multi threaded - 1000 threads - cpu usage
+// Node - single threaded - event loop
+
+// efficiency - node
+
+// 2) nodejs - simple
+
+// processes - java - nultiple thread
+// process - node
+// thread
+
+// multi threadding vs single threaded
+
+
+// 3) security - java is better
+
+
+// 4) response time - java is better
+
+// 5) stack - javascript, react - fetch, node - BE
+
+// single developer concept - javascript
+// hybrid app - 1 dev - javascript - react native
+// java based android app
+// C# based ios app
+
+
+// 6) os compatibility - java - jvm - ready to use - platform independant
+// node - separate build - system dependant
+
+// 7) java - IDE - netbeans,eclipse, AEM, intelliJ - User Communidity much wider - build, debug, plugins
+// node - VS Code
+
+
+// 8) other languages compatibility - java
+// nodejs is better
+
+
+
+// // office 365 - .NET ()
+
+// Revise - Event Loop, Promise, async await, async programming
+
+
+// -------------------------------------
+// *****Java - AEM, hibernate, spring, struts
+// ***php - wordpress(CMS), openkart-e-commerce, laravel-framework, codeigniter-framework, shopify - e -coomerce, magento(e-commerce) - adobe
+// *****python - django - framework, flask
+
+
+// .NET - ASP.NET - web  C#.NET - ios
+// ruby on rails - ROR
+// golang
+// C/C++ (incompatibility/memory management)
+
+
+
+// visual basic - os level (C/C++)
+// basic - os level
---
+++ temp.js
@@ -0,0 +1,10 @@
+const add = (a,b) => a+b;
+const mul = (a,b) => a*b;
+const div = (a,b) => a/b;
+
+module.exports.add = add;
+module.exports.mul = mul;
+module.exports.div = div;
+
+// import export - ES6
+// require - js
